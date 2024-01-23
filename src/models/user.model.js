0519223a29db63be961295ff0cd61c5be259c802
hammadansari <hamzaansaree15@gmail.com>
2024-01-24 01:14:42 +0500

    Basic Setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,21 @@
 import mongoose, { Schema } from "mongoose";
+
+
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        required: [true, 'First Name is Required'],
+        trim: true,
+    },
+    last_name: {
+        type: String,
+        trim: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['Male', 'Female', 'Custom'],
+        default: 'Male',
+    }
+})

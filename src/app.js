98c57167ebd529fb77930baaa6e0d95324a2eb15
hammadansari <muhammadhammad2134982@gmail.com>
2024-01-24 01:08:21 +0500

    Basic Setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
-import express from 'express';
-export const app = express();
+import express, { urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+const app = express();
+
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: process.env.DATA_LIMIT }));
+app.use(express.static('public'));
+app.use(urlencoded({ extended: true, limit: process.env.DATA_LIMIT }));
 
 
-import { User } from './models/user.model';
 
+
+
+
+
+
+export { app }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,11 +1,1 @@
 import mongoose, { Schema } from "mongoose";
-
-const userSchema = new Schema({
-    name: {
-        type: String,
-        unique: true,
-        required: true
-    }
-})
-
-export const User = mongoose.model('User', userSchema);

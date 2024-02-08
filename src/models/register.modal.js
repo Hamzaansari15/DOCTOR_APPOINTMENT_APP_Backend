e2101e6c6c89273dfa602f7bf1d8fbe8097ce9eb
hammadansari <hamzaansaree15@gmail.com>
2024-02-08 20:45:16 +0500

    Update Basic Setup

--- src/models/register.modal.js
+++ src/models/register.modal.js
@@ -1,25 +1,25 @@
 import mongoose, { Schema, trusted } from 'mongoose';
 
 
-// const registerSchema = new Schema({
-//     fullName: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         lowerCase: true,
-//         trim: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     accountType: {
-//         type: String,
-//         enum: ['doctor', 'user'],
-//         required: true
-//     }
-// })
+const registerSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowerCase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    accountType: {
+        type: String,
+        enum: ['doctor', 'user'],
+        required: true
+    }
+})

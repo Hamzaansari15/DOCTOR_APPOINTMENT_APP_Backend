3b9f3a83ee10436c9c2750ec37077973cb123a83
hammadansari <hamzaansaree15@gmail.com>
2024-02-12 23:35:47 +0500

    Write User and Member Models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -78,34 +78,34 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
-// userSchema.pre('save', async function () {
-//     if (!this.isModified('password')) return;
-//     return (this.password = await bcrypt.hash(this.password, 10));
-// });
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    return (this.password = await bcrypt.hash(this.password, 10));
+});
 
-// userSchema.methods.comparePassword = async function (password) {
-//     return await bcrypt.compare(password, this.password);
-// };
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
-// userSchema.methods.generateRefreshToken = function () {
-//     return jwt.sign(
-//         { _id: this._id },
-//         process.env.REFRESH_TOKEN_SECRET,
-//         { expiresIn: process.env.REFRESH_TOKENEXPIRY }
-//     );
-// };
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        { _id: this._id },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKENEXPIRY }
+    );
+};
 
-// userSchema.methods.generateAccessToken = function () {
-//     return jwt.sign(
-//         {
-//             _id: this._id,
-//             fullName: `${this.firstName} ${this.lastName}`,
-//             phoneNumber: this.phoneNumber,
-//             email: this.email,
-//             accountType: this.accountType,
-//             status: this.status
-//         },
-//         process.env.ACCESS_TOKEN_SECRET,
-//         { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-//     );
-// };
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            fullName: `${this.firstName} ${this.lastName}`,
+            phoneNumber: this.phoneNumber,
+            email: this.email,
+            accountType: this.accountType,
+            status: this.status
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+};

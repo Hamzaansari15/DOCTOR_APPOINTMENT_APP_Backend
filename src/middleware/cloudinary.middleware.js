3ae507a12b12eaf56a647a274ff0192e3ff70f5e
hammadansari <hamzaansaree15@gmail.com>
2024-02-14 01:34:08 +0500

    Write Cloudinary utils Function

---
+++ src/middleware/cloudinary.middleware.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs'
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadonCloudinary = async (localPath) => {
+    try {
+        if (!localPath) return null;
+        const response = await cloudinary.uploader.upload(localPath);
+        fs.unlinkSync(localPath);
+        return response;
+    }
+    catch (error) {
+        fs.unlinkSync(localPath);
+        return null;
+    }
+}
+
+export default uploadonCloudinary;

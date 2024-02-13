10ec349f5b8967aaf75780c62ca2f49ee16ad35b
hammadansari <hamzaansaree15@gmail.com>
2024-02-13 23:56:19 +0500

    Write Member and User Model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -87,25 +87,25 @@ userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateRefreshToken = function () {
-    return jwt.sign(
-        { _id: this._id },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: process.env.REFRESH_TOKENEXPIRY }
-    );
-};
+// userSchema.methods.generateRefreshToken = function () {
+//     return jwt.sign(
+//         { _id: this._id },
+//         process.env.REFRESH_TOKEN_SECRET,
+//         { expiresIn: process.env.REFRESH_TOKENEXPIRY }
+//     );
+// };
 
-userSchema.methods.generateAccessToken = function () {
-    return jwt.sign(
-        {
-            _id: this._id,
-            fullName: `${this.firstName} ${this.lastName}`,
-            phoneNumber: this.phoneNumber,
-            email: this.email,
-            accountType: this.accountType,
-            status: this.status
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-    );
-};
+// userSchema.methods.generateAccessToken = function () {
+//     return jwt.sign(
+//         {
+//             _id: this._id,
+//             fullName: `${this.firstName} ${this.lastName}`,
+//             phoneNumber: this.phoneNumber,
+//             email: this.email,
+//             accountType: this.accountType,
+//             status: this.status
+//         },
+//         process.env.ACCESS_TOKEN_SECRET,
+//         { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+//     );
+// };

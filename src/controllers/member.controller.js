f71eb66193dfcd5f75eef25f2dd036327ad715c8
hammadansari <hamzaansaree15@gmail.com>
2024-02-20 23:32:49 +0500

    Write Member Controller

--- src/controllers/member.controller.js
+++ src/controllers/member.controller.js
@@ -1,8 +1,21 @@
 import { Member } from "../models/member.modal.js";
 import asyncHandler from '../utils/asyncHandler.js';
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerMember = asyncHandler(async (req, res, next) => {
-    res.send('User Successful Register!')
+
+    // Check Request Data
+    console.log(req.body);
+    const { fullName, email, phoneNumber, password, accountType } = req.body;
+    if ([fullName, email, phoneNumber, password, accountType].some(item => item?.trim() === '')) {
+        console.log('error');
+        res.status(400).json(new ApiResponse('400', '', 'All Data Required'));
+    }
+    else console.log('Else');
+
+
+    // res.send('User Successful Register!')
 })
 
 

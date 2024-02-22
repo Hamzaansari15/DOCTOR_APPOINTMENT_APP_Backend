ea4ee85b31ac39e5da5e1701122cf407084b2aab
hammadansari <hamzaansaree15@gmail.com>
2024-02-23 01:07:53 +0500

    Write Member Controller and Routes and Utils Data Validator Function

--- src/controllers/member.controller.js
+++ src/controllers/member.controller.js
@@ -17,33 +17,33 @@ const generateRefreshTokenAndAccessToken = async (userId) => {
     }
 }
 
-// const registerMember = asyncHandler(async (req, res, next) => {
+const registerMember = asyncHandler(async (req, res, next) => {
 
-//     try {
-//         // Check Request Data
-//         const error = requestBodyValidator(req.body, ['fullName', 'email', 'phoneNumber', 'password', 'accountType'])
-//         if (error !== null) res.status(400).json(new ApiResponse(400, '', error, 'All fields are required!'))
+    try {
+        // Check Request Data
+        const error = requestBodyValidator(req.body, ['fullName', 'email', 'phoneNumber', 'password', 'accountType'])
+        if (error !== null) res.status(400).json(new ApiResponse(400, '', error, 'All fields are required!'))
 
-//         // Check User Already Exists
-//         const memberExists = await Member.findOne({ $or: [{ email }, { phoneNumber }] });
-//         if (memberExists) return res.status(400).json(new ApiResponse(400, '', 'User Already Exists'));
+        // Check User Already Exists
+        const memberExists = await Member.findOne({ $or: [{ email }, { phoneNumber }] });
+        if (memberExists) return res.status(400).json(new ApiResponse(400, '', 'User Already Exists'));
 
-//         // Save User in Database
-//         const member = await Member.create(req.body);
-//         if (!member) return res.status(500).json(new ApiResponse(500, '', 'Something went wrong while user register'))
-//         const { refreshToken, accessToken } = await generateRefreshTokenAndAccessToken(member._id);
-//         const savedMember = await Member.findById(member?._id).select('-password -refreshToken');
-//         res
-//             .status(201)
-//             .cookie('accessToken', accessToken).
-//             cookie('refreshToken', refreshToken)
-//             .json(new ApiResponse(201, { savedMember, refreshToken, accessToken }, 'User Successful Created'));
+        // Save User in Database
+        const member = await Member.create(req.body);
+        if (!member) return res.status(500).json(new ApiResponse(500, '', 'Something went wrong while user register'))
+        const { refreshToken, accessToken } = await generateRefreshTokenAndAccessToken(member._id);
+        const savedMember = await Member.findById(member?._id).select('-password -refreshToken');
+        res
+            .status(201)
+            .cookie('accessToken', accessToken).
+            cookie('refreshToken', refreshToken)
+            .json(new ApiResponse(201, { savedMember, refreshToken, accessToken }, 'User Successful Created'));
 
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json(new ApiResponse(500, '', 'Server Error'));
-//     }
-// })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(new ApiResponse(500, '', 'Server Error'));
+    }
+})
 
 
 export {
